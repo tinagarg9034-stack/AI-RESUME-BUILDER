2ebd3ceb7adb9b442d62ff4eb2b875eef6b4daf0
Tina Garg <tinagarg-stack.com>
2026-06-27 14:12:19 +0530

    Day 8 - Resume Form

--- app.py
+++ app.py
@@ -1,18 +1,64 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from models import db, User
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///resume.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")  
 
-@app.route("/login")
+    if request.method == "POST":
+
+        fullname = request.form["fullname"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        from models import User
+
+        new_user = User(
+            fullname=fullname,
+            email=email,
+            password=password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "Signup Successful!"
+
+    return render_template("signup.html") 
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")  
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email, password=password).first()
+
+        if user:
+            return f"Welcome, {user.fullname}!"
+        else:
+            return "Invalid Email or Password"
+
+    return render_template("login.html") 
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+    
+with app.app_context():
+    db.create_all()    
 
 if __name__ == "__main__":
     app.run(debug=True)
